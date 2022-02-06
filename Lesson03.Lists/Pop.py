bd2ac1c164e2e636fd0a844a05d3840172189c74
vanloc1808 <vanloc1808@gmail.com>
2022-02-06 15:36:47 +0700

    pop

---
+++ Lesson03.Lists/Pop.py
@@ -0,0 +1,10 @@
+cs_topics = ['Python', 'Data Structures', 'Ballon Making', 'Algorithms', 'Clowns 101']
+
+# pop the last element
+removed_element = cs_topics.pop()
+print(cs_topics)
+print(removed_element)
+
+# pop the element 'Ballon Making'
+cs_topics.pop(2)
+print(cs_topics)
