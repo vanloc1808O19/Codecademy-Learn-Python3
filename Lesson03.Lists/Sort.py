471a03a43bcd0e3a2350b0d1249732d8647c7563
vanloc1808 <vanloc1808@gmail.com>
2022-02-06 15:31:14 +0700

    sort

---
+++ Lesson03.Lists/Sort.py
@@ -0,0 +1,10 @@
+# sort() method
+example = [4, 2, 1, 3]
+example.sort()
+print(example)
+
+# sorted() function
+unsorted_list = [4, 2, 1, 3]
+sorted_list = sorted(unsorted_list)
+print(unsorted_list)
+print(sorted_list)
