fe7335e01f85281af866a72cbc87ad31f1e2b00e
vanloc1808 <vanloc1808@gmail.com>
2022-02-06 11:12:57 +0700

    variables

---
+++ Lesson01.HelloWorld/Variables.py
@@ -0,0 +1,8 @@
+# These are all valid variable names and assignment
+user_name = "@vanloc1808"
+user_id = 100
+verified = True
+
+# A variable's value can be changed after assignment
+points = 100
+points = 120
