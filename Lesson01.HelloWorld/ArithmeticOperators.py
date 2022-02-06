ed40a636807184bda662786faf0e49844170e92d
vanloc1808 <vanloc1808@gmail.com>
2022-02-06 11:10:49 +0700

    arithmetic operators

---
+++ Lesson01.HelloWorld/ArithmeticOperators.py
@@ -0,0 +1,15 @@
+# Arithmetic operations
+
+result1 = 10 + 30
+result2 = 40 - 10
+result3 = 50 * 5
+result4 = 16 / 4
+result5 = 25 % 2
+result6 = 5 ** 3
+
+# Plus - Equal operator
+counter = 0
+counter += 10
+# this operator will also perform string concatenation
+message = "Part 1 of the message "
+message += "Part 2 of the message"
