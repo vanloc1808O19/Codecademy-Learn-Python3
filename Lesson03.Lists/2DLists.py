409a2c86880dbe4b55ab6a39d75d12b2ef8d3cda
vanloc1808 <vanloc1808@gmail.com>
2022-02-06 15:06:19 +0700

    2D lists

---
+++ Lesson03.Lists/2DLists.py
@@ -0,0 +1,10 @@
+# A 2D list of people's heights
+heights = [['Noelle', 61], ['Ali', 70], ['Sam', 67]]
+
+# access the sublist at index 0, and then accest the 1st index of that sublist.
+noelle_height = heights[0][1]
+print(noelle_height)
+
+# modify the index [1][1]
+heights[1][1] = 72
+print(heights)
