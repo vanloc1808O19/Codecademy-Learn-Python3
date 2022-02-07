62f260eddc15dd50d871788af18e9a8f3ac86605
vanloc1808 <vanloc1808@gmail.com>
2022-02-07 09:49:22 +0700

    strings

---
+++ Lesson06.Strings/Strings.py
@@ -0,0 +1,8 @@
+# in CS, sequences of characters are called strings.
+
+txt = "She said \"Never let me go \"."
+print(txt)
+
+game = "Popular Nintendo Game: Mario Kant"
+print("l" in game)
+print("x" in game)
