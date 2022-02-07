e9c24a7b0c6336fc1964a97a00a6f465b33ac647
vanloc1808 <vanloc1808@gmail.com>
2022-02-07 21:47:10 +0700

    date and time

---
+++ Lesson07.Modules/DateAndTime.py
@@ -0,0 +1,10 @@
+import datetime
+
+feb_7_2022 = datetime.date(2022, 2, 7)
+print(feb_7_2022)
+
+time_21_45min_18sec = datetime.time(21, 45, 18)
+print(time_21_45min_18sec)
+
+timestamp = datetime.datetime(2022, 2, 7, 21, 45, 18)
+print(timestamp)
