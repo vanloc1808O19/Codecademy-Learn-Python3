15999f149dcf1106c7cca02112374a5b2fc38d63
vanloc1808 <vanloc1808@gmail.com>
2022-02-07 09:56:04 +0700

    strings

--- Lesson06.Strings/Strings.py
+++ Lesson06.Strings/Strings.py
@@ -6,3 +6,14 @@ print(txt)
 game = "Popular Nintendo Game: Mario Kant"
 print("l" in game)
 print("x" in game)
+
+for s in game:
+    print(s)
+
+length = len(game)
+print(length)
+
+x = "Hello "
+y = "World"
+z = x + y
+print(z)
