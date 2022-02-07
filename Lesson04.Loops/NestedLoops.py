a7cffd9bd149eb23ceb5eae375675f08e800519e
vanloc1808 <vanloc1808@gmail.com>
2022-02-07 09:19:24 +0700

    nested loops

---
+++ Lesson04.Loops/NestedLoops.py
@@ -0,0 +1,7 @@
+names = [['Jobs', 'Gates'], ['Newton', 'Euclid'], ['Einstein', 'Feynman']]
+
+# this outer loop will iterate over each list in the names list
+for name in names:
+    # this inner loop will go through each name in the list
+    for n in name:
+        print(n)
