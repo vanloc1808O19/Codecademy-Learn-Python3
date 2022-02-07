178fd60240337bdce30aa8b440a64b4a6073d9cd
vanloc1808 <vanloc1808@gmail.com>
2022-02-07 21:18:45 +0700

    import Python modules

---
+++ Lesson07.Modules/ImportPythonModules.py
@@ -0,0 +1,13 @@
+# three different ways to import modules:
+
+# first way:
+# import module
+# module.function()
+
+# second way:
+# from module import function
+# function()
+
+# third way:
+# from module import *
+# function()
