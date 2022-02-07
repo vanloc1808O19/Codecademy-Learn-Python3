450e36d01d0282754bbbc87c50e5bef4afc4f08a
vanloc1808 <vanloc1808@gmail.com>
2022-02-07 09:17:22 +0700

    while loop

---
+++ Lesson04.Loops/WhileLoop.py
@@ -0,0 +1,11 @@
+# this loop will run 1 time only
+hungry = True
+while hungry:
+    print('I am hungry!')
+    hungry = False
+    
+# this loop will run 5 times
+i = 1
+while i < 6:
+    print(i)
+    i += 1
